6c0aff4bfeb23acf7ee92771398dc3cd02ac92fb
oreste <muhirwa.g.oreste@gmail.com>
2022-10-21 04:30:13 +0200

    Les procedures sont separés dans des fonctions

--- src/main/java/apachecamel/HelloWorld.java
+++ src/main/java/apachecamel/HelloWorld.java
@@ -1,35 +1,38 @@
 package apachecamel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class HelloWorld {
 
 	public static void main(String[] args) throws Exception {
-		// Hello world
+		
+
+	}
+
+	static void helloWorld()throws Exception{
 		CamelContext context=new DefaultCamelContext();
 		context.addRoutes(new HelloWorldRoute());
 		System.out.println("Hello apache");
 
+	}
 
-
-		/**
-		 * Chaque fichier dans le repertoire "source" sera recopier immediatemment dans le repertoire "destination" 
-		 * Cette procédure prend place quand la boucle while est entrain de tourner avec context.start
-		 */
+	/**
+	 * Chaque fichier dans le repertoire "source" sera recopier immediatemment dans le repertoire "destination" 
+	 * Cette procédure prend place quand la boucle while est entrain de tourner avec context.start
+	 */
+	static void copierFichier()throws Exception{
+		
 		CamelContext context2=new DefaultCamelContext();
 		context2.addRoutes(new FileCopyRoute() );
-		
 		while(true){
 			context2.start();
 		}
-
-
-		/**
-		 * 
-		 */
-
 	}
+
+	
 
 }
