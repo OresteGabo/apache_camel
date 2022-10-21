88e8dc91166798275d1bb8b4f3734586ddc70856
oreste <muhirwa.g.oreste@gmail.com>
2022-10-21 03:20:36 +0200

    Les fichier sont copier d'une repertoire a l'autre, chaque fichier creer à la main est copier automatiquement

--- src/main/java/apachecamel/HelloWorld.java
+++ src/main/java/apachecamel/HelloWorld.java
@@ -1,15 +1,37 @@
 package apachecamel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class HelloWorld {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+		// Hello world
 		CamelContext context=new DefaultCamelContext();
 		context.addRoutes(new HelloWorldRoute());
 		System.out.println("Hello apache");
+
+
+
+		/**
+		 * Chaque fichier dans le repertoire "source" sera recopier immediatemment dans le repertoire "destination" 
+		 * Cette procédure prend place quand la boucle while est entrain de tourner avec context.start
+		 */
+		CamelContext context2=new DefaultCamelContext();
+		context2.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				from("file:source?noop=true").to("file:destination");		
+			}
+			
+		});
+		
+		while(true){
+			context2.start();
+		
+		}
 
 	}
 
